68dcfa6c157774962886aff684b0c142168a315a
paradisewu <2226068236@qq.com>
2016-09-20 16:36:12 +0800

    wuyang

---
+++ TTUIFramework-master/Assets/Sample/GameManager/GameMain.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TinyTeam.UI;
+
+public class GameMain : MonoBehaviour
+{
+
+    void Start()
+    {
+        TTUIPage.ShowPage<UITopBar>();
+        TTUIPage.ShowPage<UIMainPage>();
+    }
+
+    void Update()
+    {
+
+    }
+
+}
